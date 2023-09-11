5372854481adb5018e5d988ce52d9ae4b4bdb771
Souaad OULEDJIA <souadouledjia@gmail.com>
2023-09-11 12:29:39 -0700

    4. Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,12 +3,10 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return list(my_list)
-
     new_list = []
     for i in range(len(my_list)):
         if i == idx:
             new_list.append(element)
         else:
             new_list.append(my_list[i])
-    
     return new_list
