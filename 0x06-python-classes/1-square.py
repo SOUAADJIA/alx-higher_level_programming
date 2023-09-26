0e5eef39004107f5c679bbe3b1568c206a48ad49
Souaad OULEDJIA <souadouledjia@gmail.com>
2023-09-26 13:54:30 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define an initialization method for size."""
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size):
+        """Initializes the square with a given size."""
+        self.__size = size
