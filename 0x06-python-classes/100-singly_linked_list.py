1b6e022398b5be4f42d97a6e88f8421403bb7b0f
Souaad OULEDJIA <souadouledjia@gmail.com>
2023-09-26 17:31:47 -0700

    7. Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""Define node in a singly linked list."""
-
 
 class Node:
     """Represents a node in a singly linked list."""
@@ -34,22 +32,12 @@ class Node:
             raise TypeError('next_node must be a Node object')
         self.__next_node = value
 
-
 class SinglyLinkedList:
     """Represents a singly linked list."""
 
     def __init__(self):
         """Initialize an empty linked list."""
         self.head = None
-
-    def to_string(self):
-        """Convert the linked list to a string for printing."""
-        string = ''
-        current = self.head
-        while current:
-            string += str(current.data) + '\n'
-            current = current.next_node
-        return string[:-1]
 
     def sorted_insert(self, value):
         """Insert a new Node into the correct sorted position."""
@@ -75,3 +63,14 @@ class SinglyLinkedList:
 
         new_node.next_node = current.next_node
         current.next_node = new_node
+
+    def __str__(self):
+        """Convert the linked list to a string for printing."""
+        string = ''
+        current = self.head
+
+        while current:
+            string += str(current.data) + '\n'
+            current = current.next_node
+
+        return string.strip()
