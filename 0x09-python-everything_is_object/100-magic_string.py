73a86bc24a9108b6a1a4da8fe07c6048f36318ef
SOUAADJIA <souadouledjia@gmail.com>
2023-10-03 18:25:33 +0000

    29. #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("BestSchool")
-    return ", ".join(l)
+def magic_string(mylist=[]):
+    mylist.append("BestSchool")
+    return ", ".join(mylist)
