761b2bd7691a114beaa7427ad426131716c58080
SOUAADJIA <souadouledjia@gmail.com>
2023-10-05 01:48:48 +0000

    5. Max integer - Unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,30 +5,29 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """Test cases for max_integer function."""
 
-    def test_regular_list(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    def test_integers(self):
+        """Test valid integers."""
+        self.assertEqual(max_integer([10, 20, 30, 40]), 40)
+        self.assertEqual(max_integer([50]), 50)
+        self.assertEqual(max_integer([-5, -10, -15]), -5)
+        self.assertEqual(max_integer([1, 3, 5, 7]), 7)
+        self.assertEqual(max_integer([0, 0, 0, 0]), 0)
+        self.assertEqual(max_integer([-2, -6, -8, -4]), -2)
 
-    def test_unordered_list(self):
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+    def test_floats(self):
+        """Test valid floats."""
+        self.assertEqual(max_integer([1.5, 2.5, 3.5, 4.5]), 4.5)
+        self.assertEqual(max_integer([-1.1, -2.2, -3.3]), -1.1)
+        self.assertEqual(max_integer([0.5, -0.1, 0.0]), 0.5)
 
-    def test_empty_list(self):
-        self.assertIsNone(max_integer([]))
-
-    def test_single_element_list(self):
-        self.assertEqual(max_integer([42]), 42)
-
-    def test_negative_numbers(self):
-        self.assertEqual(max_integer([-1, -3, -2, -4]), -1)
-
-    def test_float_numbers(self):
-        self.assertEqual(max_integer([1.5, 3.7, 2.2, 4.9]), 4.9)
-
-    def test_mixed_data_types(self):
-        self.assertEqual(max_integer([1, "hello", 2.5, True]), 2.5)
-
-    def test_duplicate_max(self):
-        self.assertEqual(max_integer([5, 5, 5, 5]), 5)
+    def test_other_types(self):
+        """Test other compatible data types."""
+        self.assertEqual(max_integer([1, 2, 3, float('inf')]), float('inf'))
+        self.assertEqual(max_integer([1, 2, 3, float('nan')]), 3)
+        self.assertEqual(max_integer(), None)
+        self.assertEqual(max_integer([True, False, True]), True)
 
 
 if __name__ == '__main__':
