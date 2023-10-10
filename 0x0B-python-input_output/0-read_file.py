2590ce0ac7745e4c41490ca729c6d8c2600d8b46
Souaad OULEDJIA <souadouledjia@gmail.com>
2023-10-10 10:01:03 -0700

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module containing a function to read and print the content of a file.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to be read.
+        Default is an empty string.
+
+    Returns:
+        None
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
