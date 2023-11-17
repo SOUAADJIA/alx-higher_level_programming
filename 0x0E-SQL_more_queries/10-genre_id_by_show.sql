74efc7f5590631f88570a20d81f459308e8e8cbb
Souaad OULEDJIA <souadouledjia@gmail.com>
2023-11-16 16:11:56 -0800

    10. Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Listing shows with at least one linked genre in the hbtn_0d_tvshows database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
