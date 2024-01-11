e99e17dd61e076f6984757296e5a5f92d5d19ade
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-01-10 16:10:11 -0800

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
+
 const list = require('./100-data').list;
+
+const mappedList = list.map((value, index) => value * index);
+
 console.log(list);
-const map = list.map((x, i) => x * i);
-console.log(map);
+console.log(mappedList);
