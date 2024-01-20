cb19d7e3c2cbca383bab7c665abca98ad4c5712e
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-01-19 16:32:07 -0800

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,5 +11,5 @@ class State(Base):
     """Class representing the 'states' table."""
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
