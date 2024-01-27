610ce69d845a3eda68192ad08a407dedc993f660
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-01-26 16:17:54 -0800

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,9 @@ This script takes in a URL, sends a request, and displays the value of the X-Req
 import urllib.request
 import sys
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    x_request_id = response.getheader("X-Request-Id")
-    print(x_request_id)
+    with urllib.request.urlopen(url) as response:
+        x_request_id = dict(response.headers).get("X-Request-Id")
+        print(x_request_id)
