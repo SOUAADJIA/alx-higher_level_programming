4dcdce077d65f94b07f3ea0616963428cdf65fc7
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-01-26 16:12:53 -0800

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,12 @@ This script fetches https://alx-intranet.hbtn.io/status using urllib.
 
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print(f"    - type: {type(body)}")
+print(f"    - content: {body}")
+print(f"    - utf8 content: {body.decode('utf-8')}")
