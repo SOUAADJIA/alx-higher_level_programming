0a20e87baead7f4c513d3d0409a56c437e1f51d8
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-03-26 18:32:15 -0700

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,27 @@
 
 const request = require('request');
 
-request(process.argv[2], (error, response, body) => {
+// Get the API URL from command line arguments
+const apiUrl = process.argv[2];
+
+const characterId = '18';
+
+request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
-    return;
+    console.error(error);
   }
-  
-  // Count the occurrences of '/people/18/' in the response body
-  const count = (body.match(/\/people\/18\//g) || []).length;
-  
-  console.log(count);
+
+  const data = JSON.parse(body);
+
+  let movieCount = 0;
+
+  // Iterate through each movie
+  data.results.forEach(movie => {
+    // Check if Wedge Antilles is present in the characters array
+    if (movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+      movieCount++;
+    }
+  });
+
+  console.log(movieCount);
 });
