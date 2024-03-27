dd02a72b907ba0ce544f86603ec108d02cc22b47
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-03-26 19:09:10 -0700

    task 7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,33 +5,21 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    console.error(err);
     return;
   }
+  const data = JSON.parse(body);
 
-  const movieData = JSON.parse(body);
-  const charactersUrls = movieData.characters;
-
-  const fetchCharacter = (characterUrl) => {
-    return new Promise((resolve, reject) => {
-      request.get(characterUrl, (error, response, body) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        const characterData = JSON.parse(body);
-        resolve(characterData.name);
-      });
+  data.characters.forEach(characterUrl => {
+    request(characterUrl, (err, response, body) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
     });
-  };
-
-  Promise.all(charactersUrls.map(fetchCharacter))
-    .then(characters => {
-      characters.forEach(character => console.log(character));
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  });
 });
