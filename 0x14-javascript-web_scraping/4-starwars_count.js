98ef5c77dcbf2968c29bf7bdcda52ead2711433e
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-03-26 18:35:50 -0700

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,15 +5,21 @@ const request = require('request');
 // Get the API URL from command line arguments
 const apiUrl = process.argv[2];
 
+// Character ID for Wedge Antilles
 const characterId = '18';
 
+// Make a GET request to the Star Wars API
 request.get(apiUrl, (error, response, body) => {
+  // Check for errors
   if (error) {
     console.error(error);
+    return;
   }
 
+  // Parse the JSON response
   const data = JSON.parse(body);
 
+  // Initialize a counter for movies with Wedge Antilles
   let movieCount = 0;
 
   // Iterate through each movie
@@ -24,5 +30,6 @@ request.get(apiUrl, (error, response, body) => {
     }
   });
 
+  // Print the number of movies with Wedge Antilles
   console.log(movieCount);
 });
